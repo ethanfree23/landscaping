75d6d17a83df92f5dfddbc0597acdb2b86dba821
Ethan Freeman <ethanfree23@gmail.com>
2022-06-11 22:35:13 -0500

    added some pages, ran into issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
+import HomePage from './Pages/HomePage'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <HomePage />
+    </>
   )
 }
 
---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from '../Components/HeroSection'
+
+function HomePage() {
+  return (
+    <>
+    <div>HomePage</div>
+    {/* <HeroSection /> */}
+    </>
+  )
+}
+
+export default HomePage
